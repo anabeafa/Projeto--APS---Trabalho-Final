0344affc86e25932bd0dc8d9bf4950d415a0a4ff
Ana Beatriz Farias <129701826+anabeafa@users.noreply.github.com>
2023-12-07 12:24:02 -0300

    Atualizar o MyDbContext.cs

--- Models/MyDbContext.cs
+++ Models/MyDbContext.cs
@@ -8,5 +8,14 @@ namespace ProjetoAna.Models
        {
        }
        public DbSet<Cliente> Cliente {get; set;}
+       public DbSet<Vendedor> Vendendor {get; set;}
+       public DbSet<Produto> Produto {get;set;}
+       public DbSet<Marca> Marca {get; set;}
+       public DbSet<Item> Item {get; set;}
+       public DbSet<Pagamento>Pagamento {get; set;}
+       public DbSet<TipoDePagamento>TipoDePagamento {get;set;}
+       public DbSet<NotaDeVenda> NotaDeVenda{get; set;}
+       public DbSet<Transportadora> Transportadora {get; set;}
+  
    }
 }
